55f6810c5a85ab7fe37a72b650cbf97e970dd95c
Valera Leontyev <feedbee@gmail.com>
2024-02-16 20:35:53 +0000

    Arguments for the CLI app; Google provider max input

--- bin/ai-read-it-cli.js
+++ bin/ai-read-it-cli.js
@@ -3,16 +3,27 @@
 const { AiReadIt, createProvider } = require('../lib/ai-read-it');
 const { Readable } = require('stream');
 
-// Initialize the provider
-let providerName = 'OpenAI'; // default provider
-const providerArgIndex = process.argv.findIndex(arg => arg === '--provider' || arg === '-p');
-if (providerArgIndex !== -1 && process.argv.length > providerArgIndex + 1) {
-    providerName = process.argv[providerArgIndex + 1];
+// Commandline arguments
+const providerName = getCmdArg('-p', '--provider') || 'OpenAI'; // default provider
+const ttsOptions = {};
+for (let key of ['model', 'language', 'voice', 'response_format', 'speed', 'pitch', 'is_ssml']) {
+    const opt = getCmdArg(null, '--tts.' + key);
+    if (opt) {
+        ttsOptions[key] = opt;
+    }
 }
+const providerOptions = {};
+for (let key of ['detect_language']) {
+    const opt = getCmdArg(null, '--p.' + key);
+    if (opt) {
+        providerOptions[key] = opt;
+    }
+}
+
 // Provider is initialized with the API Key from the environment:
-// - Open AI: process.env.OPENAI_API_KEY
+// - OpenAI: process.env.OPENAI_API_KEY
 // - Google: process.env.GOOGLE_APPLICATION_CREDENTIALS_JSON
-const aiReadIt = new AiReadIt(createProvider(providerName));
+const aiReadIt = new AiReadIt(createProvider(providerName, null, providerOptions));
 
 // Read input text from stdin
 let inputText = '';
@@ -24,9 +35,17 @@ process.stdin.on('data', (chunk) => {
 process.stdin.on('end', async () => {
     try {
         // Convert text to speech
-        const readable = Readable.from(await aiReadIt.largeTextToSpeech(inputText));
+        const readable = Readable.from(await aiReadIt.largeTextToSpeech(inputText, ttsOptions));
         readable.pipe(process.stdout);
     } catch (error) {
         console.error("Error:", error);
     }
 });
+
+function getCmdArg(argShortName, argFullName) {
+    const argIndex = process.argv.findIndex(arg => arg === argShortName || arg === argFullName);
+    if (argIndex !== -1 && process.argv.length > argIndex + 1) {
+        return process.argv[argIndex + 1];
+    }
+    return undefined;
+}
